7067ef84cb04ac0a5da9edc65eba07d015dce2a8
Tanishq Shivasharan <155131699+Tanishq747Shivasharan@users.noreply.github.com>
2025-03-16 11:00:37 +0530

    tanscommit

--- MicroProject.java
+++ MicroProject.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class MicroProject extends JFrame {
     private JPanel lightP;
-    private JButton redb, yellowb, grrenb;
+    private JButton redb, yellowb, greenb;
     private Color currentLight = Color.RED;
 
     public MicroProject (){
@@ -15,6 +15,18 @@ public class MicroProject extends JFrame {
                 System.exit(0);
             }
         });
+
+        JPanel bPanel = new JPanel();
+        redb = new JButton("Red");
+        yellowb = new JButton("yellow");
+        Greenb = new JButton("Green");
+
+        // Traffic lights code goes here...
+
+        // Adding the signal buttons to the frame.
+        bPanel.add(redb);
+        bPanel.add(yellowb);
+        bPanel.add(greenb);
 
         setSize(300,400);
         setLayout(new BorderLayout());
