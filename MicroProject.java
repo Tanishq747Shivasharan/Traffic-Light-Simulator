2fc18350132d9533432fcbd7cbb3897ae754d3e2
Tanishq Shivasharan <155131699+Tanishq747Shivasharan@users.noreply.github.com>
2025-03-16 10:55:50 +0530

    tanscommit

--- MicroProject.java
+++ MicroProject.java
@@ -3,13 +3,21 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MicroProject extends JFrame {
+    private JPanel lightP;
+    private JButton redb, yellowb, grrenb;
+    private Color currentLight = Color.RED;
+
     public MicroProject (){
+        setTitle("Traffic Light Simulator");
 
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+
+        setSize(300,400);
+        setLayout(new BorderLayout());
     }
 
     public static void main(String[] args){
