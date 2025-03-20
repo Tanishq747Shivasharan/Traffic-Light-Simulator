02ee184295565fab3d1dff25f8cca4c0e09d6461
Tanishq Shivasharan <155131699+Tanishq747Shivasharan@users.noreply.github.com>
2025-03-20 12:36:52 +0530

    Update MicroProject.java

--- MicroProject.java
+++ MicroProject.java
@@ -10,10 +10,12 @@ public class MicroProject extends JFrame {
     private Timer t;
     private JButton startStopB;
     private boolean isRunning = false;
+    private Image i;
 
     public MicroProject() {
-        ImageIcon img = new ImageIcon("\"D:\\SignalIcon.png\""); // work on image icon PLS
-        img.setDescription("TANS");
+        ImageIcon img = new ImageIcon("SignalIcon.png");
+        setIconImage(img.getImage());
+        i = new ImageIcon("road.jpg").getImage();
         setTitle("Traffic Light Simulator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
@@ -28,6 +30,8 @@ public class MicroProject extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
+                g.drawImage(i, 0, 0, getWidth(), getHeight(),this);
+
                 g.setColor(Color.BLACK);
                 g.fillRect(100, 50, 100, 300);
 
@@ -43,7 +47,7 @@ public class MicroProject extends JFrame {
         };
         add(lightP, BorderLayout.CENTER);
 
-        startStopB = new JButton("Start Simulator");
+        startStopB = new JButton("Stop Simulator");
         startStopB.setFont(new Font("Consolas", Font.BOLD, 16));
         // I have added an underscore insted of using e as it will supress the e parameter given for the action event.
         startStopB.addActionListener(_ -> toggleSimulator());
@@ -54,6 +58,8 @@ public class MicroProject extends JFrame {
         setSize(500, 500);
         setVisible(true);
     }
+
+    // private void drawChou
 
     // To control(by user)
     private void toggleSimulator() {
